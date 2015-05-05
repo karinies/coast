d8a933091d6601f0ba7f4414ad1338dac09881bc
kari <kari@ics.uci.edu>
2015-05-05 15:01:45 -0700

    New code for generating risk data events.

---
+++ examples/market-notify/events/risk-events-simulator/gen_risk_events.py
@@ -0,0 +1,93 @@
+'''
+This module generates stock risk events for the COAST market-notify example
+It takes an input file of the format:
+
+<number of events to generate>
+<min time delay> <max time delay>
+[<stock symbol> <start risk_value> <delta change> ]*
+
+e.g.
+
+200
+1 5
+GOOG 3 1
+YHOO 10 2
+FB 15 3
+IBM 20 4
+
+Sample generated output will look like this:
+
+delay 3
+IBM risk 20
+delay 4
+FB risk 15
+delay 1
+IBM risk 20
+delay 2
+GOOG risk 4
+delay 5
+YHOO risk 8
+delay 1
+GOOG risk 3
+delay 2
+IBM risk 22
+delay 2
+YHOO risk 7
+delay 2
+IBM risk 19
+delay 1
+
+'''
+
+import random
+import time
+
+event_count = 0
+max_delay = min_delay = 0
+stock_risks = {}
+
+def init_stock_data(infile):
+    # read event count
+    event_count = int(infile.readline()[:-1]);
+    # read min and max delays
+    min_delay_str, max_delay_str = infile.readline()[:-1].split(" ");
+    min_delay = int(min_delay_str)
+    max_delay = int(max_delay_str)
+
+    for line in infile:
+        stock_info = line[:-1].split(" "); # shave \n off end of line
+        init_risk_value = int(stock_info[1])
+        delta_change = int(stock_info[2])
+        stock_risks[stock_info[0]] = [init_risk_value, delta_change]
+
+    return event_count, min_delay, max_delay, stock_risks
+
+def generate_stock_events(outfile, event_count, min_delay, max_delay, stocks):
+    for i in range(event_count):
+        delay = random.randint(min_delay, max_delay)
+        symbol = random.choice(list(stocks.keys()))
+        current_risk= stocks[symbol][0]
+        delta = stocks[symbol][1]
+        diff = random.randint(-delta, delta)
+        new_risk = current_risk+ diff
+        stocks[symbol][0] = new_risk
+        print("delay %d" % delay, file=outfile)
+        print("%s %s %d" % (symbol, "risk", new_risk), file=outfile)    
+
+def main():
+    in_filename = "risk_input.txt"
+    infile = open(in_filename, "r")
+    event_count, min_delay, max_delay, stock_risks = init_stock_data(infile)
+    print("event count:", event_count)
+    print("min delay:", min_delay, "max_delay:", max_delay)    
+    print("Risk info:", stock_risks)
+    
+    out_filename = "risk_events.txt"
+    outfile = open(out_filename, "w")
+    generate_stock_events(outfile, event_count, min_delay, max_delay, stock_risks)
+    infile.close()
+    outfile.close()
+
+    
+if __name__ == "__main__":
+    main()
