3f1bed92b7a5e1bd5dabff9939546e9149a3f3f4
kari <kari@ics.uci.edu>
2015-05-07 16:39:37 -0700

    Added min and max risk values. fixed error, was assuming \n at the end of line in both stock and risk event generator.

--- examples/market-notify/events/risk-events-simulator/gen_risk_events.py
+++ examples/market-notify/events/risk-events-simulator/gen_risk_events.py
@@ -3,12 +3,14 @@ This module generates stock risk events for the COAST market-notify example
 It takes an input file of the format:
 
 <number of events to generate>
+<min risk value> <max risk value>
 <min time delay> <max time delay>
 [<stock symbol> <start risk_value> <delta change> ]*
 
 e.g.
 
 200
+0 100
 1 5
 GOOG 3 1
 YHOO 10 2
@@ -49,42 +51,51 @@ stock_risks = {}
 def init_stock_data(infile):
     # read event count
     event_count = int(infile.readline()[:-1]);
+    # read min and max risk values
+    min_risk_str, max_risk_str = infile.readline()[:-1].split(" ");
+    min_risk = int(min_risk_str)
+    max_risk = int(max_risk_str)
     # read min and max delays
     min_delay_str, max_delay_str = infile.readline()[:-1].split(" ");
     min_delay = int(min_delay_str)
     max_delay = int(max_delay_str)
 
     for line in infile:
-        stock_info = line[:-1].split(" "); # shave \n off end of line
+        stock_info = line.replace("\n", "").split(" ");
         init_risk_value = int(stock_info[1])
         delta_change = int(stock_info[2])
         stock_risks[stock_info[0]] = [init_risk_value, delta_change]
 
-    return event_count, min_delay, max_delay, stock_risks
+    return event_count, min_risk, max_risk, min_delay, max_delay, stock_risks
 
-def generate_stock_events(outfile, event_count, min_delay, max_delay, stocks):
+def generate_stock_events(outfile, event_count, min_risk, max_risk, \
+                          min_delay, max_delay, stocks):
     for i in range(event_count):
         delay = random.randint(min_delay, max_delay)
         symbol = random.choice(list(stocks.keys()))
         current_risk= stocks[symbol][0]
         delta = stocks[symbol][1]
         diff = random.randint(-delta, delta)
-        new_risk = current_risk+ diff
-        stocks[symbol][0] = new_risk
+        new_risk = current_risk + diff
+        if min_risk <= new_risk <= max_risk:
+            stocks[symbol][0] = new_risk
         print("delay %d" % delay, file=outfile)
-        print("%s %s %d" % (symbol, "risk", new_risk), file=outfile)    
+        print("%s %s %d" % (symbol, "risk", stocks[symbol][0] ), file=outfile)    
 
 def main():
     in_filename = "risk_input.txt"
     infile = open(in_filename, "r")
-    event_count, min_delay, max_delay, stock_risks = init_stock_data(infile)
+    event_count, min_risk, max_risk, min_delay, max_delay, stock_risks = \
+        init_stock_data(infile)
     print("event count:", event_count)
-    print("min delay:", min_delay, "max_delay:", max_delay)    
+    print("min risk value:", min_risk, "max risk value:", max_risk)
+    print("min delay:", min_delay, "max delay:", max_delay)    
     print("Risk info:", stock_risks)
     
     out_filename = "risk_events.txt"
     outfile = open(out_filename, "w")
-    generate_stock_events(outfile, event_count, min_delay, max_delay, stock_risks)
+    generate_stock_events(outfile, event_count, min_risk, max_risk,\
+                          min_delay, max_delay, stock_risks)
     infile.close()
     outfile.close()
 
