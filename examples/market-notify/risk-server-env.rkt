1adfbf69be2299d500e66f39790525e92f88b7aa
kari <kari@ics.uci.edu>
2015-05-27 14:30:26 -0700

    Binding environment for risk server.  Includes functions to register for risk events and count the current number of subscriptions.

---
+++ examples/market-notify/risk-server-env.rkt
@@ -0,0 +1,36 @@
+#lang racket/base
+
+(require
+  "../../baseline.rkt"
+  "../../persistent/environ.rkt"
+  "./risk-server-register.rkt"
+  "../../promise.rkt"
+  "../../murmur.rkt"
+  "../../send.rkt"
+  "../../transport/gate.rkt"
+  "../../transport/access.rkt"
+  "../../islet.rkt"
+  "../../curl/base.rkt"
+  "../../spawn.rkt")
+
+(provide
+ MARKET/SERVER/ENV)
+
+;; Still incomplete: time, date, gates.
+(define MARKET/SERVER/ENV
+  (pairs-to-environ
+   BASELINE
+   (list
+    ; Hack for now.
+    (define/global/1 'display display) ; HACK, HACK, HACK.
+    (define/global/2 'register risk/subscribe)
+    (define/global/0 'risk/subs/count risk/subs/count)
+    (define/global/1 'duplet/resolver duplet/resolver)
+    (define/global/1 'duplet/block duplet/block)
+    (define/global/1 'murmur/payload murmur/payload)
+    (define/global/N 'islet/curl/new islet/curl/new)
+    (define/global/0 'this/islet/nickname this/islet/nickname)
+    (cons 'GATE/ALWAYS GATE/ALWAYS)
+    (cons 'INTRA INTRA)
+    (define/global/2 'send send)
+    )))
