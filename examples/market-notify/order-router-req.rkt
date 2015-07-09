b3489bbdc47bd1b459ecab5de8cd9279a2b6cb05
kari <kari@ics.uci.edu>
2015-07-09 16:52:12 -0700

    Fixed: forgot to provide order-exec-report/unit-price.
    Tried creating a new trader-request struct that includes and order and a curl, but had problems getting the nested struct to send properly.  For now, have embedded curl in the order-request structure.

--- examples/market-notify/order-router-req.rkt
+++ examples/market-notify/order-router-req.rkt
@@ -9,12 +9,20 @@
          order-request/unit-price
          order-request/quantity
          order-request/uid
+         order-request/curl
+         ; add a curl here?  no, create new struct that has an order request object and a curl
+         )
+
+(provide trader-request
+         trader-request/order
+         trader-request/curl
          )
 
 (provide order-exec-report
          order-exec-report/uid
          order-exec-report/symbol
          order-exec-report/status  
+         order-exec-report/unit-price 
          order-exec-report/qnty-requested
          order-exec-report/qnty-filled        
          )
@@ -29,9 +37,15 @@
    symbol     ; Stock symbol
    unit-price ; The price per share
    quantity   ; The amount of shares to be traded.
-   uid)       ; Unique id given to order
+   uid        ; Unique id given to order
+   curl)      ; Curl to used to transmit order-exect-reports back to the trader
   #:transparent)
 
+(struct trader-request
+  (order ; order-request struct
+   curl) ; curl to used to transmit order-exect-reports back to the trader
+  #:transparent)
+   
   
 (struct order-exec-report
   (uid            ; Unique id given to order
@@ -42,6 +56,10 @@
    qnty-filled)   ; The amount of shares that are traded so far
   #:transparent)
 
+(struct/getters/define order-request sender target symbol unit-price quantity uid curl)
+(struct/getters/define trader-request order curl)
+(struct/getters/define order-exec-report uid symbol status unit-price qnty-requested qnty-filled)
+
 
 (define (vector->order-request v)
   (cond 
@@ -51,8 +69,9 @@
            [symbol (vector-ref v 3)]
            [unit-price (vector-ref v 4)]
            [quantity (vector-ref v 5)]
-           [uid (vector-ref v 6)])
-       (order-request sender target symbol unit-price quantity uid))]
+           [uid (vector-ref v 6)]
+           [curl (vector-ref v 7)])
+       (order-request sender target symbol unit-price quantity uid curl))]
     [else
      (display "VECTOR IS NOT ORDER REQUEST")(display "\n")]))
 
@@ -70,8 +89,6 @@
      (display "VECTOR IS NOT ORDER EXECUTION REPORT")(display "\n")]))
 
 
-(struct/getters/define order-request sender target symbol unit-price quantity uid)
-(struct/getters/define order-exec-report uid symbol status unit-price qnty-requested qnty-filled)
 
 
 
