7facecfa926e74d5a38cf52bcafe03955cf9978a
Matias <matias.giorgio@gmail.com>
2015-07-24 19:02:22 -0700

    Adding receive-failed event.

--- comet/comet-types.rkt
+++ comet/comet-types.rkt
@@ -5,10 +5,13 @@
  COMET/CURL/SEND
  COMET/CURL/RECEIVE
  COMET/CURL/TRANSFER
- COMET/ISLAND/START)
+ COMET/ISLAND/START
+ COMET/CURL/RECEIVE/FAILED)
 
 (define COMET/CURL/NEW "curl-new")
 (define COMET/CURL/SEND "curl-send")
 (define COMET/CURL/RECEIVE "curl-receive")
 (define COMET/CURL/TRANSFER "curl-transfer")
 (define COMET/ISLAND/START "island-start")
+;(define COMET/CURL/RECEIVE/REVOKED "curl-receive-revoked")
+(define COMET/CURL/RECEIVE/FAILED "curl-receive-failed") ; Message wasn't received.
