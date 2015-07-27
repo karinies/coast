35183eb5d78f166f8f61b1c2c3d173e852437ae7
Matias <matias.giorgio@gmail.com>
2015-07-26 17:42:57 -0700

    Adding functions to print order-requests and order-exec-reports nicely.

--- examples/examples-env.rkt
+++ examples/examples-env.rkt
@@ -3,7 +3,9 @@
 (require
   "../baseline.rkt"
   "../persistent/environ.rkt"
-  "examples-base.rkt")
+  "../islet.rkt"
+  "examples-base.rkt"
+  "market-notify/order-router-req.rkt")
 
 (provide EXAMPLES/ENVIRON)
 
@@ -15,4 +17,9 @@
     (define/global/N 'islet/log/error islet/log/error)
     (define/global/N 'islet/log/debug islet/log/debug)
     (define/global/N 'islet/log/warning islet/log/warning)
+    (define/global/N 'environ/merge environ/merge)
+    (define/global/1 'islet/environ islet/environ)
+    (define/global/0 'this/islet this/islet)
+    (define/global/1 'order-request/pretty order-request/pretty)
+    (define/global/1 'order-exec-report/pretty order-exec-report/pretty)
     )))
