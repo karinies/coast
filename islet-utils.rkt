e6d013300632a5d8c70226bed6b9de283e437d63
Matias <matias.giorgio@gmail.com>
2015-07-26 17:42:16 -0700

    Setting worker callback CURL as INTER instead of ANYWHERE.

--- islet-utils.rkt
+++ islet-utils.rkt
@@ -28,7 +28,7 @@
     (islet/jumpstart
      x
      (lambda ()
-       (let ([d (islet/curl/new (list nickname) GATE/ALWAYS #f ANYWHERE)]) ; Create a duplet that the worker will use to receive messages.
+       (let ([d (islet/curl/new (list nickname) GATE/ALWAYS #f INTER)]) ; Create a duplet that the worker will use to receive messages.
          (send (promise/resolver p/for/curl) (duplet/resolver d)) ; Send the new curl to the parent.
          (let loop ([m (duplet/block d)]) ; Listen for new messages.
            (when (murmur? m) ; When a murmur is received (it should happen every time)
