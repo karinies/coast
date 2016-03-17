51cb32f57353821b588078ef3eab99c32e784441
Matias Giorgio <matias.giorgio@gmail.com>
2016-03-16 18:19:52 -0700

    info.rkt to create Motile-Island package.

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection 'multi)
+(define deps '("Motile-Island"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/Motile-Island.scrbl" ())))
+(define pkg-desc "Motile/Island implementation of the COmputationAl State Transfer (COAST) architectural style")
+(define version "0.0")
+(define pkg-authors '("Michael Gorlick"))
