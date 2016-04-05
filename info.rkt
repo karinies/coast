2ccd1301ad4046b1ad5ec4a9678bb8d517cd248e
Matias Giorgio <matias.giorgio@gmail.com>
2016-04-04 17:47:35 -0700

    Removed dependencies and set version to 0.9.

--- info.rkt
+++ info.rkt
@@ -2,8 +2,6 @@
 (define collection "Island")
 (define deps '("racket/base"
                "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/Motile-Island.scrbl" ())))
 (define pkg-desc "Motile/Island implementation of the COmputationAl State Transfer (COAST) architectural style")
-(define version "0.0")
+(define version "0.9")
 (define pkg-authors '("Michael Gorlick"))
