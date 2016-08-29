6877f1f32e606f60c24182d19b46c86174581724
kari <kari@ics.uci.edu>
2016-08-29 13:51:25 -0700

    Removed extraneous dependencies.

--- examples/lunar-lander/sim-server-env.rkt
+++ examples/lunar-lander/sim-server-env.rkt
@@ -9,7 +9,6 @@
   "../../send.rkt"
   "../../transport/gate.rkt"
   "../../islet.rkt"
-  "../../islet-utils.rkt"
   "../../uuid.rkt"
   "../examples-env.rkt")
 
@@ -20,31 +19,21 @@
   (pairs-to-environ
    EXAMPLES/ENVIRON
    (list
-    ; Hack for now.
-    (define/global/1 'display display) ; HACK, HACK, HACK.
+    ; display functions
+    (define/global/1 'display display)
     (define/global/0 'newline newline)
     
     ; vector functions
-    (define/global/1 'vector? vector?)
     (define/global/N 'vector vector)
     (define/global/2 'vector-ref vector-ref)
-    (define/global/3 'vector-set! vector-set!)
-    (define/global/1 'struct->vector struct->vector)
-    (define/global/N 'vector-append vector-append)
     
+    ; COAST functions/declarations
     (define/global/1 'duplet/resolver duplet/resolver)
     (define/global/1 'duplet/block duplet/block)
     (define/global/1 'murmur/payload murmur/payload)
     (define/global/N 'islet/curl/new islet/curl/new)
-    (define/global/0 'this/islet/nickname this/islet/nickname)
-    (define/global/N 'motile/call motile/call)
-    (define/global/3 'subislet/callback/new subislet/callback/new)
-    (define/global/0 'uuid/symbol uuid/symbol)
     (define/global/1 'sleep sleep)
-    (cons 'BASELINE/SPAWN BASELINE/SPAWN)
     (cons 'GATE/ALWAYS GATE/ALWAYS)
-    (cons 'INTRA INTRA)
-    (cons 'EXAMPLES/ENVIRON EXAMPLES/ENVIRON)
     (define/global/2 'send send)
     
     )))
